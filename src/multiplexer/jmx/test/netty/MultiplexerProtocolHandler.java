ba68e68d7b5248705ff71f60c4153d8c9215ca7f
Piotr Findeisen <piotr.findeisen@gmail.com>
2009-05-30 03:36:28 +0200

    netty example

---
+++ src/multiplexer/jmx/test/netty/MultiplexerProtocolHandler.java
@@ -0,0 +1,36 @@
+package multiplexer.jmx.test.netty;
+
+import multiplexer.Multiplexer.MultiplexerMessage;
+
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelPipelineCoverage;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+
+@ChannelPipelineCoverage("all")
+class MultiplexerProtocolHandler extends SimpleChannelHandler {
+
+	@Override
+	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
+		System.err.println("messageReceived");
+		assert e.getMessage() instanceof MultiplexerMessage : e.getMessage()
+			+ " is not a MultiplexerMessage";
+		System.err.println(e.getMessage());
+	}
+
+	@Override
+	public void writeRequested(ChannelHandlerContext ctx, MessageEvent e)
+		throws Exception {
+
+		assert e.getMessage() instanceof MultiplexerMessage : "You should feed the channel with MultiplexerMessages, not "
+			+ e.getMessage();
+		super.writeRequested(ctx, e);
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+		e.getCause().printStackTrace();
+		e.getChannel().close();
+	}
+}
---
+++ src/multiplexer/jmx/test/netty/SimpleNettyConnection.java
@@ -0,0 +1,219 @@
+package multiplexer.jmx.test.netty;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+
+import multiplexer.Multiplexer;
+import multiplexer.Multiplexer.MultiplexerMessage;
+import multiplexer.Multiplexer.WelcomeMessage;
+
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelFutureListener;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.socket.SocketChannel;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
+import org.jboss.netty.handler.codec.protobuf.ProtobufDecoder;
+import org.jboss.netty.handler.codec.protobuf.ProtobufEncoder;
+
+import com.google.protobuf.ByteString;
+
+public class SimpleNettyConnection {
+
+	static Logger logger = Logger.getAnonymousLogger();
+
+	private final long instanceId;
+	private boolean connected = false;
+	private boolean connecting = false;
+
+	private SocketChannel channel;
+
+	public SimpleNettyConnection() {
+		instanceId = new Random().nextLong();
+	}
+
+	public SimpleNettyConnection(ChannelFactory factory, SocketAddress address)
+		throws InterruptedException {
+
+		this();
+		asyncConnect(factory, address).await();
+	}
+
+	public ChannelFuture asyncConnect(ChannelFactory factory,
+		SocketAddress address) {
+		assert !connected;
+		assert !connecting;
+		connecting = true;
+
+		ClientBootstrap bootstrap = new ClientBootstrap(factory);
+
+		bootstrap.setOption("tcpNoDelay", true);
+		bootstrap.setOption("keepAlive", true);
+
+		ChannelPipeline pipeline = bootstrap.getPipeline();
+
+		// Encoders
+		pipeline.addLast("rawMessageEncoder", new RawMessageCodecs.RawMessageEncoder());
+		pipeline.addLast("multiplexerMessageEncoder", new ProtobufEncoder());
+
+		// Decoders
+		pipeline.addLast("rawMessageDecoder", new RawMessageCodecs.RawMessageFrameDecoder());
+		pipeline.addLast("multiplexerMessageDecoder", new ProtobufDecoder(
+			Multiplexer.MultiplexerMessage.getDefaultInstance()));
+
+		// Protocol handler
+		pipeline.addLast("multiplexerProtocolHandler",
+			new MultiplexerProtocolHandler());
+
+		ChannelFuture connectOperation = bootstrap.connect(address);
+		connectOperation.addListener(new ChannelFutureListener() {
+
+			@Override
+			public void operationComplete(ChannelFuture future)
+				throws Exception {
+
+				System.err.println("connected");
+				assert future.isDone();
+				assert !connected;
+				assert connecting;
+				connected = true;
+				connecting = false;
+				channel = (SocketChannel) future.getChannel();
+			}
+		});
+		return connectOperation;
+	}
+
+	long getInstanceId() {
+		return instanceId;
+	}
+
+	class SendingResult {
+		ChannelFuture future;
+		long messageId;
+
+		public SendingResult(ChannelFuture future, long messageId) {
+			this.future = future;
+			this.messageId = messageId;
+		}
+	}
+
+	public SendingResult sendMessage(ByteString message, int type)
+		throws IOException {
+		MultiplexerMessage mxmsg = MultiplexerMessage.newBuilder().setId(
+			new Random().nextLong()).setFrom(getInstanceId()).setType(type)
+			.setMessage(message).build();
+
+		return new SendingResult(channel.write(mxmsg), mxmsg.getId());
+	}
+
+	// private byte[] read_all(int len) throws IOException {
+	// byte[] buffer = new byte[len];
+	// int off = 0;
+	// while (off < len) {
+	// off += input_stream.read(buffer, off, len - off);
+	// }
+	// return buffer;
+	// }
+	//	
+	// public MultiplexerMessage receive_message() throws IOException {
+	// ByteBuffer buffer = ByteBuffer.wrap(read_all(HEADER_LENGTH));
+	// buffer.order(ByteOrder.LITTLE_ENDIAN);
+	// int length = buffer.getInt();
+	// int crc32 = buffer.getInt();
+	// byte[] body = read_all(length);
+	// CRC32 body_sum = new CRC32();
+	// body_sum.update(body);
+	// assert (int)body_sum.getValue() == crc32;
+	// MultiplexerMessage mxmsg =
+	// MultiplexerMessage.newBuilder().mergeFrom(body).build();
+	// System.out.println("received "+body.length+" bytes ("+mxmsg.getMessage().size()+" meaningful)");
+	// return mxmsg;
+	// }
+	
+
+	private void close() {
+		channel.close().awaitUninterruptibly();
+	}
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public static void main(String[] args) throws IOException,
+		InterruptedException {
+
+		final int PYTHON_TEST_SERVER = 106;
+		final int CONNECTION_WELCOME = 2;
+		final int MULTIPLEXER = 1;
+		final int PYTHON_TEST_REQUEST = 110;
+
+		ChannelFactory factory = new NioClientSocketChannelFactory(Executors
+			.newCachedThreadPool(), Executors.newCachedThreadPool());
+		SimpleNettyConnection c = new SimpleNettyConnection(factory,
+			new InetSocketAddress("localhost", 1980));
+
+		// send out invitation
+		System.out.println("sending welcome message");
+		ByteString message = WelcomeMessage.newBuilder().setType(
+			PYTHON_TEST_SERVER).setId(c.getInstanceId()).build().toByteString();
+		c.sendMessage(message, CONNECTION_WELCOME).future.await();
+		
+		
+		c.close();
+		factory.releaseExternalResources();
+
+		// // receive the invitation
+		// System.out.println("waiting for welcome message");
+		// MultiplexerMessage mxmsg = c.receive_message();
+		// System.out.println("validating welcome message");
+		// assert mxmsg.getType() == CONNECTION_WELCOME;
+		// WelcomeMessage peer =
+		// WelcomeMessage.newBuilder().mergeFrom(mxmsg.getMessage()).build();
+		// assert peer.getType() == MULTIPLEXER;
+		// peer.getId();
+		//
+		// // send a stupid search_query
+		// ArrayList<Byte> sq = new ArrayList<Byte>();
+		// for (byte d:
+		// "this is a search query with null (\\x00) bytes and other ".getBytes())
+		// sq.add(d);
+		// for (int i = Byte.MIN_VALUE; i <= Byte.MAX_VALUE; i++)
+		// sq.add((byte)i);
+		//	    
+		// Output sqo = ByteString.newOutput();
+		// for (byte d: sq)
+		// sqo.write(d);
+		//	    
+		// System.out.println("sending sample search query");
+		// long id = c.sendMessage(sqo.toByteString(),
+		// PYTHON_TEST_REQUEST).messageId;
+		// System.out.println("waiting for sample search query");
+		// mxmsg = c.receive_message();
+		// System.out.println("validating sample search query");
+		// assert mxmsg.getId() == id;
+		// assert mxmsg.getType() == PYTHON_TEST_REQUEST;
+		// assert mxmsg.getMessage().equals(sqo.toByteString());
+
+		// # send a large search_query
+		// query = open("/dev/urandom", "r").read(1024 * 1024)
+		// print "sending large search query"
+		// id = c.send_message(query, types.SEARCH_QUERY)
+		// print "waiting for large search query"
+		// msg = c.receive_message()
+		// print "validating large search query"
+		// assert msg.id == id
+		// assert msg.type == types.SEARCH_QUERY
+		// assert msg.message == query
+
+	}
+
+}
+
+
