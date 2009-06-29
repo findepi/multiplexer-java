d1a926bb8015d5ed6c85c624b68a21c1e1fa9de7
Kasia Findeisen <kasia.findeisen@gmail.com>
2009-06-30 00:22:45 +0200

    test Query

--- src/multiplexer/jmx/test/TestConnectivity.java
+++ src/multiplexer/jmx/test/TestConnectivity.java
@@ -1,16 +1,9 @@
-/**
- * 
- */
 package multiplexer.jmx.test;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
-
-import org.jboss.netty.channel.ChannelFuture;
-
-import com.google.protobuf.ByteString;
 
 import junit.framework.TestCase;
 import multiplexer.Multiplexer.MultiplexerMessage;
@@ -21,6 +14,11 @@ import multiplexer.jmx.IncomingMessageData;
 import multiplexer.jmx.JmxClient;
 import multiplexer.jmx.SendingMethod;
 import multiplexer.jmx.exceptions.NoPeerForTypeException;
+import multiplexer.jmx.exceptions.OperationFailedException;
+
+import org.jboss.netty.channel.ChannelFuture;
+
+import com.google.protobuf.ByteString;
 
 /**
  * @author Kasia Findeisen
@@ -59,8 +57,9 @@ public class TestConnectivity extends TestCase {
 		assertNotSame(msgSent, msgReceived);
 	}
 
-	public void testBackend() throws UnknownHostException, InterruptedException, NoPeerForTypeException {
-		
+	public void testBackend() throws UnknownHostException,
+		InterruptedException, NoPeerForTypeException {
+
 		ByteString msgBody = ByteString.copyFromUtf8("Więcej budynió!");
 
 		// create backend
@@ -89,7 +88,7 @@ public class TestConnectivity extends TestCase {
 		builder.setType(Types.TEST_REQUEST).setMessage(msgBody);
 		MultiplexerMessage msgSent = client.createMessage(builder);
 		assertFalse(msgSent.hasTo());
-		
+
 		// send message
 		ChannelFuture sendingOperation = client.send(msgSent,
 			SendingMethod.THROUGH_ONE);
@@ -103,12 +102,52 @@ public class TestConnectivity extends TestCase {
 		assertNotSame(msgSent, msgReceived);
 		assertEquals(msgReceived.getType(), msgSent.getType());
 		assertEquals(msgReceived.getMessage(), msgBody);
-		
+
 		// cleanup
 		backend.cancel();
 		backendThread.join(3000);
 		assertFalse(backendThread.isAlive());
-		if (backendThread.isAlive())  {
+		if (backendThread.isAlive()) {
+			backendThread.interrupt();
+		}
+	}
+
+	public void testQuery() throws UnknownHostException,
+		OperationFailedException, NoPeerForTypeException, InterruptedException {
+
+		// create backend
+		AbstractBackend backend = new AbstractBackend(Peers.TEST_SERVER) {
+			@Override
+			protected void handleMessage(MultiplexerMessage message)
+				throws Exception {
+				// reply with the same message, directly to the sender
+				reply(createResponse(message.getType(), message.getMessage()));
+			}
+		};
+
+		// connect backend and run in new thread
+		backend
+			.connect(new InetSocketAddress(InetAddress.getLocalHost(), 1980));
+		Thread backendThread = new Thread(backend);
+		backendThread.setName("backend main thread");
+		backendThread.start();
+
+		// connect
+		JmxClient client = new JmxClient(Peers.TEST_CLIENT);
+		client.connect(new InetSocketAddress(InetAddress.getLocalHost(), 1980));
+
+		// query
+		IncomingMessageData msgData = client.query(ByteString
+			.copyFromUtf8("Lama ma kota."), Types.TEST_REQUEST, 2000);
+
+		assertEquals(msgData.getMessage().getMessage(), ByteString
+			.copyFromUtf8("Lama ma kota."));
+
+		// cleanup
+		backend.cancel();
+		backendThread.join(3000);
+		assertFalse(backendThread.isAlive());
+		if (backendThread.isAlive()) {
 			backendThread.interrupt();
 		}
 	}
