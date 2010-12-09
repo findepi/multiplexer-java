2a4ce4ee007a0af6d5e9f16058c3236fd5b75961
Piotr Findeisen <piotr.findeisen@gmail.com>
2010-12-09 12:52:58 +0100

    regenerated constant files

--- src/multiplexer/protocol/Constants.java
+++ src/multiplexer/protocol/Constants.java
@@ -1,18 +1,3 @@
-// Copyright 2009 Warsaw University, Faculty of Physics
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package multiplexer.protocol;
 
 import java.util.*;
@@ -20,7 +5,7 @@ import java.util.*;
 public class Constants implements multiplexer.jmx.tools.rulesconsts.ConstantsPack {
 
 	public static class PeerTypes implements multiplexer.jmx.tools.rulesconsts.PeerTypes {
-		
+
 		public static final PeerTypes instance = new PeerTypes();
 
 		public final static int MULTIPLEXER = 1;
@@ -63,7 +48,7 @@ public class Constants implements multiplexer.jmx.tools.rulesconsts.ConstantsPac
 	}
 
 	public static class MessageTypes implements multiplexer.jmx.tools.rulesconsts.MessageTypes {
-		
+
 		public static final MessageTypes instance = new MessageTypes();
 
 		public final static int PING = 1;
